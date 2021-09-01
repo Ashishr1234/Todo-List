505039e60d7b6033656c2574936e1db5c53eaaba
Ashish Kumar Rastogi <ashish.rastogi183@gmail.com>
2021-08-31 23:49:43 -0700

    Todo List

---
+++ todos.js
@@ -0,0 +1,36 @@
+let input = prompt('what would you like to do ?');
+const todos = ['Collect Chicken eggs','Clean Litter Box'];
+while(input!=='quit' && input!=='q')
+{
+    if(input==='list')
+    {
+    console.log('************');
+    for(let i=0;i<todos.length;i++)
+    {
+        console.log(`${i}: ${todos[i]}`);
+    }
+    console.log('*************');
+    }
+    else if(input==='new')
+    {
+        const newTodo = prompt('OK! what is the newTodo ?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list !`);
+    }
+    else if(input==='delete')
+    {
+        const indexStr = prompt('Ok, enter an index to delete');
+        const index = parseInt(indexStr);
+        if(!(Number.isNaN(index)))
+        {
+        const deleted = todos.splice(index,1);
+        console.log(`Ok, deleted ${deleted[0]}`);
+        }
+    else{
+        console.log('Unknown index');
+    }
+    }
+
+    input = prompt("what would you like to do ?");
+}
+console.log("ok! quit the app");
